f09a401767280a930bdf754bb7a9e105496b34ad
vmrosti <vojtech.razdik@gmail.com>
2025-07-25 14:02:28 +0200

    Update update-config.php

--- update-config.php
+++ update-config.php
@@ -1,9 +1,13 @@
 <?php
-if (isset($_GET['source']) && in_array($_GET['source'], ['google', 'excel'])) {
-  $data = ['source' => $_GET['source']];
+header("Content-Type: text/plain");
+
+$input = json_decode(file_get_contents('php://input'), true);
+
+if (isset($input['source']) && in_array($input['source'], ['google', 'excel'])) {
+  $data = ['source' => $input['source']];
   file_put_contents('config.json', json_encode($data, JSON_PRETTY_PRINT));
   echo "OK";
 } else {
   http_response_code(400);
-  echo "Neplatný parametr.";
+  echo "Neplatný vstup.";
 }
